2b57cd65b1e0a019a0fddcde4851e20a826a1b14
joaoguilherme <alcajoao1@gmail.com>
2024-10-16 22:20:38 -0300

    Changes on icon

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const app = require('path');
 const app = express();
 
 // Define a porta
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
 // Rota principal
 app.get('/', (req, res) => {
@@ -10,6 +11,20 @@ app.get('/', (req, res) => {
 });
 
 // Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+// Define o diretório público para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para servir a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html')); // Serve o arquivo 'index.html'
+});
+
+// Porta do servidor
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
