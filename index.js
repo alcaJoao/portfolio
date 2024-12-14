683d09008f07422e47484d208b7da013bfc643c8
joaoguilherme <alcajoao1@gmail.com>
2024-12-14 04:45:36 -0300

    mudança principal: ao clicar em finalizar compra é redirecionado para o whatsapp

---
+++ index.js
@@ -0,0 +1,13 @@
+// Importa as bibliotecas
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config(); // Carrega variáveis de ambiente do arquivo .env
+
+// Configura o Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_URL.split('@')[1], // Extrai o Cloud Name
+  api_key: process.env.CLOUDINARY_URL.split('://')[1].split(':')[0], // Extrai o API Key
+  api_secret: process.env.CLOUDINARY_URL.split(':')[2].split('@')[0], // Extrai o API Secret
+});
+
+// Testa a configuração com um log
+console.log('Cloudinary configurado:', cloudinary.config());
